9bbe35f16bd271b308dc5155a98527fca1855fb3
Tyuiu.KolganenkoRV@outlook.com <kolganenko.rinat@mail.ru>
2025-09-10 21:36:44 +0500

    Task2

---
+++ Tyuiu.KolganenkoRV.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.Serialization.Json;
+using Tyuiu.KolganenkoRV.Sprint0.Task2.V0.Lib;
+
+Console.WriteLine(DataService.GetMessage("Игорь")); 
+Console.ReadKey();
